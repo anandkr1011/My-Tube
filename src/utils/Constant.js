8937b56bc2b88b753e6fbfb54d4bfb33c193936f
Anand kumar <anandku1011@gmail.com>
2023-10-16 01:17:33 +0530

    remove google API key

--- src/utils/Constant.js
+++ src/utils/Constant.js
@@ -1,4 +1,4 @@
-const GOOGLE_API_KEY = "AIzaSyB46_mFbZUAdbbtbSsP_gyytdBuizU_nNA";
+const GOOGLE_API_KEY = "you should write your google api key";
 
 export const YOUTUBE_VIDEOS_API =
   "https://youtube.googleapis.com/youtube/v3/videos?part=snippet%2CcontentDetails%2Cstatistics&chart=mostPopular&maxResults=50&regionCode=IN&key=" +
